c06e618bd8bc247308cdf4d2e2058d4925f0b717
Nycto <James@RoundEights.com>
2013-12-04 08:52:27 -0800

    Refactor SessionProvder for testability

--- src/main/scala/session/Prereq.scala
+++ src/main/scala/session/Prereq.scala
@@ -2,7 +2,7 @@ package com.roundeights.tubeutil.session
 
 import scala.concurrent._
 import scala.util.{Success, Failure}
-import com.roundeights.skene.{Provider, Bundle, Request, Cookie}
+import com.roundeights.skene.{Provider, Bundle, Request, Response, Cookie}
 
 /** A loaded session */
 trait SessionReq {
@@ -14,93 +14,125 @@ trait SessionReq {
     override def toString = "Session(%s)".format( session.sessionID )
 }
 
+/** Logic for loading session data */
+private[session] object SessionLoad {
+
+    /** The default session timeout */
+    private[session] val defaultTtl = 30 * 24 * 60 * 60
+
+    /** Session validation data access*/
+    class Access (
+        private val data: DataLayer,
+        private val request: Request,
+        private val response: Response,
+        private val prototype: Cookie,
+        val isSecure: Boolean
+    ) (
+        implicit context: ExecutionContext
+    ) {
+
+        /** Extracts the session ID from the request */
+        def sessionId: Option[SessionId] = {
+            request.cookies.first( prototype.name )
+                .flatMap( cookie => SessionId.parse(cookie.value) )
+        }
+
+        /** Fetches the given sequence */
+        def getSessionInfo( sessId: SessionId ): Future[Option[SessionInfo]]
+            = data.fetch( sessId.sequenceId )
+
+        /** Sets a cookie with the given session Id */
+        def cookie( sessionId: SessionId ): Unit = {
+            response.cookie( prototype.set(
+                value = sessionId.toString,
+                httpOnly = true,
+                secure = isSecure
+            ) )
+        }
+
+        /** Completes with the given session Id */
+        def complete( sessId: SessionId ): Future[Session]
+            = Future.successful(new Session(sessId, data))
+
+        /** Creates a new session */
+        def create: Future[Session] = {
+            val sessInfo = SessionInfo.create( isSecure )
+            data.create( sessInfo )
+                .map( _ => cookie( sessInfo.sessionId ) )
+                .flatMap( _ => complete( sessInfo.sessionId ) )
+        }
+
+        /** Destroys the session and creates a new one */
+        def recreate ( sessId: SessionId ) : Future[Session] = {
+            val created = create
+            data.destroy( sessId.sequenceId ).flatMap( _ => created )
+        }
+
+        /** Returns whether the given session is expired */
+        def isExpired( sessInfo: SessionInfo ): Boolean
+            = sessInfo.isExpired( prototype.ttl.getOrElse( defaultTtl ) )
+    }
+
+    /** Processes a session */
+    def process
+        ( access: Access )
+        ( implicit context: ExecutionContext )
+    : Future[Session] = {
+        access.sessionId match {
+            case None => access.create
+            case Some(sessId) => access.getSessionInfo(sessId).flatMap {
+                case None => access.create
+                case Some(sessInfo) => {
+
+                    // If the full session ID doesn't match
+                    if ( sessInfo.sessionId != sessId ) {
+                        access.recreate( sessId )
+                    }
+
+                    // If the session is expired
+                    else if ( access.isExpired( sessInfo ) ) {
+                        access.recreate( sessId )
+                    }
+
+                    // If there is a mismatch between secure and non-secure
+                    else if ( access.isSecure ^ sessInfo.isHttps ) {
+                        access.recreate( sessId )
+                    }
+
+                    // Otherwise, the session is valid
+                    else {
+                        access.complete( sessId )
+                    }
+                }
+            }
+        }
+    }
+}
+
 /**
  * Builds a SessionReq
  */
 class SessionProvider (
     private val data: DataLayer,
     private val prototype: Cookie
-        = Cookie("sess", "", Some(30 * 24 * 60 * 60)),
+        = Cookie("sess", "", Some(SessionLoad.defaultTtl)),
     private val isSecure: (Request) => Boolean = _.isSecure
 ) (
     implicit context: ExecutionContext
 ) extends Provider[SessionReq] {
 
-    /** Extracts the session ID from the request */
-    private def extractSessionId ( bundle: Bundle ): Option[SessionId] = {
-        bundle.request.cookies.first( prototype.name )
-            .flatMap( cookie => SessionId.parse(cookie.value) )
-    }
-
-    /** Sets a cookie with the given session Id */
-    private def cookie( bundle: Bundle, sessionId: SessionId ): Unit = {
-        bundle.response.cookie( prototype.set(
-            value = sessionId.toString,
-            httpOnly = true,
-            secure = isSecure( bundle.request )
-        ) )
-    }
-
     /** {@inheritDoc} */
     override def build( bundle: Bundle, next: Promise[SessionReq] ): Unit = {
-
-        // Handle future failures
-        def run[T]( future: Future[T] )( onSuccess: (T) => Unit ): Unit = {
-            future.onComplete {
-                case Success(value) => onSuccess(value)
-                case Failure(err) => next.failure(err)
-            }
-        }
-
-        // Completes this request with the given session Id
-        def complete( sessId: SessionId ): Unit = next.success(new SessionReq {
-            override val session = new Session( sessId, data )
-        })
-
-        // Creates a new session
-        def create: Unit = {
-            val sessInfo = SessionInfo.create( isSecure(bundle.request) )
-            run( data.create( sessInfo ) )(_ => {
-                cookie( bundle, sessInfo.sessionId )
-                complete( sessInfo.sessionId )
+        next.completeWith(
+            SessionLoad.process(
+                new SessionLoad.Access(
+                    data, bundle.request, bundle.response,
+                    prototype, isSecure( bundle.request )
+                )
+            ).map(sess => new SessionReq {
+                override val session = sess
             })
-        }
-
-        // Destroys the session and creates a new one
-        def recreate( sessId: SessionId ): Unit
-            = run( data.destroy( sessId.sequenceId ) )( _ => create )
-
-        extractSessionId(bundle) match {
-            case None => create
-            case Some(sessId) => run( data.fetch(sessId.sequenceId) )(_ match {
-                case None => create
-                case Some(sessInfo) => {
-
-                    // If the full session ID doesn't match
-                    if ( sessInfo.sessionId != sessId ) {
-                        recreate( sessId )
-                    }
-
-                    // If the session is expired
-                    else if (
-                        prototype.ttl.map( sessInfo.isExpired _ )
-                            .getOrElse(false)
-                    ) {
-                        recreate( sessId )
-                    }
-
-                    // If there is a mismatch between secure and non-secure
-                    else if ( isSecure(bundle.request) ^ sessInfo.isHttps ) {
-                        recreate( sessId )
-                    }
-
-                    // Otherwise, the session is valid
-                    else {
-                        complete( sessId )
-                    }
-                }
-            })
-        }
+        )
     }
 }
 
---
+++ src/test/scala/session/PrereqTest.scala
@@ -0,0 +1,144 @@
+package com.roundeights.tubeutil.session
+
+import org.specs2.mutable._
+import org.specs2.mock._
+
+import scala.concurrent._
+import java.util.concurrent.Executor
+import java.util.Date
+import com.roundeights.skene._
+
+class PrereqTest extends Specification with Mockito {
+
+    /** An execution context that runs in the calling thread */
+    implicit val context = ExecutionContext.fromExecutor(new Executor {
+        override def execute( command: Runnable ): Unit = command.run
+    })
+
+    val sessId = SessionId()
+
+    "An Access instance" should {
+
+        "Return the session ID from the cookie" in {
+            val request = mock[Request]
+            request.cookies returns CookieJar(Cookie("sess", sessId.toString))
+
+            new SessionLoad.Access(
+                mock[DataLayer], request, mock[Response],
+                Cookie("sess", ""), true
+            ).sessionId must_== Some(sessId)
+        }
+
+        "Return None when asked for a session and it isn't set" in {
+            val request = mock[Request]
+            request.cookies returns CookieJar()
+
+            new SessionLoad.Access(
+                mock[DataLayer], request, mock[Response],
+                Cookie("sess", ""), true
+            ).sessionId must_== None
+        }
+
+        "Send a cookie when told to" in {
+            val response = mock[Response]
+
+            new SessionLoad.Access(
+                mock[DataLayer], mock[Request], response,
+                Cookie("sess", ""), true
+            ).cookie( sessId )
+
+            there was one(response).cookie( Cookie(
+                "sess", sessId.toString,
+                httpOnly = true, secure = true) )
+        }
+    }
+
+    "Loading a session" should {
+
+        val sess = new Session( sessId, mock[DataLayer] )
+
+        val sessInfo = SessionInfo( sessId, new Date, true )
+
+        val err: Throwable = new IllegalStateException("Expected error")
+
+        "Create a new session if the request doesn't have one" in {
+            val access = mock[SessionLoad.Access]
+            access.sessionId returns None
+            access.create returns Future.failed( err )
+            SessionLoad.process( access ).failed must ===(err).await
+        }
+
+        "Fail if the session creation fails" in {
+            val access = mock[SessionLoad.Access]
+            access.sessionId returns None
+            access.create returns Future.successful( sess )
+            SessionLoad.process( access ) must ===(sess).await
+        }
+
+        "Create a new one if session Id doesn't exist in the data layer" in {
+            val access = mock[SessionLoad.Access]
+            access.sessionId returns Some( sessId )
+            access.getSessionInfo( sessId ) returns Future.successful(None)
+            access.create returns Future.successful( sess )
+            SessionLoad.process( access ) must ===(sess).await
+        }
+
+        "Fail if fetching session info fails" in {
+            val access = mock[SessionLoad.Access]
+            access.sessionId returns Some( sessId )
+            access.getSessionInfo( sessId ) returns Future.failed(err)
+            SessionLoad.process(access).failed must ===(err).await
+        }
+
+        "Fail if creation fails after a session id is found and rejected" in {
+            val access = mock[SessionLoad.Access]
+            access.sessionId returns Some( sessId )
+            access.getSessionInfo( sessId ) returns Future.successful(None)
+            access.create returns Future.failed(err)
+            SessionLoad.process(access).failed must ===(err).await
+        }
+
+        "Destroy and recreate if the full session Id doesn't match" in {
+            val access = mock[SessionLoad.Access]
+            access.sessionId returns Some( sessId )
+            access.getSessionInfo( sessId ) returns Future.successful(
+                Some( SessionInfo( SessionId(), new Date, true ) ) )
+            access.recreate( sessId ) returns Future.successful( sess )
+            SessionLoad.process( access ) must ===(sess).await
+        }
+
+        "Destroy and recreate if the session is expired" in {
+            val access = mock[SessionLoad.Access]
+            access.sessionId returns Some( sessId )
+            access.getSessionInfo( sessId ) returns
+                Future.successful( Some(sessInfo) )
+            access.isExpired( sessInfo ) returns true
+            access.recreate( sessId ) returns Future.successful( sess )
+            SessionLoad.process( access ) must ===(sess).await
+        }
+
+        "Destroy and recreate if the http/https of a session is mismatched" in {
+            val access = mock[SessionLoad.Access]
+            access.sessionId returns Some( sessId )
+            access.getSessionInfo( sessId ) returns
+                Future.successful( Some(sessInfo) )
+            access.isExpired( sessInfo ) returns false
+            access.isSecure returns false
+            access.recreate( sessId ) returns Future.successful( sess )
+            SessionLoad.process( access ) must ===(sess).await
+        }
+
+        "Use a session if everything looks good" in {
+            val access = mock[SessionLoad.Access]
+            access.sessionId returns Some( sessId )
+            access.getSessionInfo( sessId ) returns
+                Future.successful( Some(sessInfo) )
+            access.isExpired( sessInfo ) returns false
+            access.isSecure returns true
+            access.complete( sessId ) returns Future.successful( sess )
+            SessionLoad.process( access ) must ===(sess).await
+        }
+    }
+}
+
+
