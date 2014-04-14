dc4078f479002a5db56b44acd3d5831814c2ad96
Nycto <James@RoundEights.com>
2014-04-13 19:08:02 -0700

    Fix DateGen thread safety
    It's not thread safe? Really? Wow. Just wow.

--- src/main/scala/DateGen.scala
+++ src/main/scala/DateGen.scala
@@ -1,6 +1,6 @@
 package com.roundeights.tubeutil
 
-import java.text.SimpleDateFormat
+import java.text.{SimpleDateFormat, DateFormat}
 import java.util.{Date, TimeZone}
 
 /**
@@ -9,14 +9,20 @@ import java.util.{Date, TimeZone}
 object DateGen {
 
     /** The standardized date format for parsing and formatting */
-    private val fmt = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ")
-    fmt.setTimeZone( TimeZone.getTimeZone("GMT") )
+    // DateFormat is not thread safe, so we need to deal with that
+    private val fmt = new ThreadLocal[DateFormat]() {
+        override protected def initialValue(): DateFormat = {
+            val date = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ")
+            date.setTimeZone( TimeZone.getTimeZone("GMT") )
+            date
+        }
+    };
 
     /** Parses a string to a date */
-    def parse ( date: String ): Date = fmt.parse( date )
+    def parse ( date: String ): Date = fmt.get.parse( date )
 
     /** Converts a date to a string */
-    def format ( date: Date ): String = fmt.format( date )
+    def format ( date: Date ): String = fmt.get.format( date )
 
     /** Formats the current time */
     def formatNow: String = format( new Date )
